d5083602b1488eef6d5b6346f991be68a234a67e
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-03-29 15:59:38 +0900

    new comments

--- src/client/app.js
+++ src/client/app.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 
-
-// import { requestLoginSuccess, getToken } from './redux/authentication/actions'
-
 import routes from './config/routes'
 import { store } from './config/store'
 
 if (global) global.System = { import () {} }
-
-// fetch token upon first render of store
-// if (getToken()) {
-//   console.log(jwtDecode(getToken()))
-//   store.dispatch(requestLoginSuccess(getToken))
-// }
 
 const App = () => {
   return (
