f69c0be27bf343a75e30f09d0c7aa63c8a57fb69
therealedsheenan <private.sheenantenepre@gmail.com>
2017-10-01 14:48:19 +0900

    add idea

--- src/client/redux/greeting/types.js
+++
@@ -1,5 +0,0 @@
-// action types for greeting
-
-export const GET_GREETING = 'GREETING_REQUEST'
-export const GET_GREETING_ERROR = 'GREETING_ERROR'
-export const GET_GREETING_SUCCESS = 'GREETING_SUCCESS'
